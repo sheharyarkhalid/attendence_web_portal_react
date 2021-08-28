6f8aae64dbd4f6d684828ce9b500bde2acd644d9
Sheharyar Khalid <sheharyarkhalid0313@gmail.com>
2021-08-28 16:07:23 +0500

    Add files via upload

---
+++ script/Ecommerce.js
@@ -0,0 +1,7 @@
+   $('.open_close_nav').click(function(){
+    $('.responsive_nav_bar').toggleClass('responsive_bar');
+    $('.content_body_wrapper').toggleClass('responsive_content_body');
+    $('.responsive_content_color').toggleClass('responsive_content_body_color');
+    });
+    
+   
